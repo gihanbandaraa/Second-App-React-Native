0ab13e3d9a4c9e8a24f62760576bfc3cecfe7e8e
Gihan Bandara <63912749+Gihan1329@users.noreply.github.com>
2024-03-02 18:41:38 +0530

    Update FancyCard

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,19 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function FancyCard() {
-  return (
-    <View>
-      <Text style={styles.headingText}>FancyCard</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text style={styles.headingText}>Treading Places</Text>
+            <View style={[styles.card, styles.cardElevated]}>
+                <Image
+                    source={{
+                        uri: 'https://us.lakpura.com/cdn/shop/files/sigiriya.jpg?v=1657769923&width=3840'
+                    }}
+                    style={styles.cardImage}
+                />
+                <View style={styles.cardBody}>
+                    <Text style={styles.cardTitle}>Sigiriya Rock</Text>
+                    <Text style={styles.cardLable}>Dambulla,Matale</Text>
+                    <Text style={styles.cardDescription}>Sigiriya or Sinhagiri
+                        is an ancient rock fortress located in the northern
+                        Matale District near the town of
+                        Dambulla in the Central Province, Sri Lanka.</Text>
+                     <View style={styles.test}> 
+                    <Text style={styles.cardFooter}>12min Away</Text>
+                    </View>  
+                </View>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     headingText: {
         fontSize: 20,
-        marginTop:8,
+        marginTop: 8,
         marginHorizontal: 8,
         fontWeight: 'bold'
+    },
+    card: {
+        width: 350,
+        height: 360,
+        marginHorizontal: 26,
+        marginVertical: 16,
+        borderRadius: 8
+    },
+    cardElevated: {
+        backgroundColor:'#d9d9d9'
+    },
+    cardImage: {
+        height: 180,
+        marginBottom:10
+    },
+    cardBody: {
+        flex:1,
+        flexGrow:1,
+        paddingHorizontal:20
+    },
+    cardTitle: {
+        fontSize:22,
+        fontWeight:'bold',
+        marginBottom:6
+    },
+    cardLable: {
+        fontSize:14,
+        fontWeight:'400'
+    },
+    cardDescription: {
+        fontSize:12,
+        marginVertical:8,
+        color:'black'
+    },
+    test:{
+        flex:1,
+        alignItems:'flex-end',
+        justifyContent:'center'
+    },
+    cardFooter: {
+        fontSize:14,
+        fontWeight:'bold',
+     
     }
 })
