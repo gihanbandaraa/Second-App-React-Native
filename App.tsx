6434bb29adbd5bda9074c942e516f44a310b5a4e
Gihan Bandara <63912749+Gihan1329@users.noreply.github.com>
2024-03-02 17:11:26 +0530

    Elevated card is completed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import FlatCards from './components/FlatCards'
+import ElevatedCard from './components/ElevatedCard'
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <FlatCards />
+        <FlatCards/>
+        <ElevatedCard/>
       </ScrollView>
     </SafeAreaView>
   )
---
+++ components/ElevatedCard.tsx
@@ -0,0 +1,69 @@
+import { Text, StyleSheet, View, ScrollView } from 'react-native'
+import React, { Component } from 'react'
+
+export default class ElevatedCard extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.headingText}>Elevated Card</Text>
+        <ScrollView horizontal={true} style={styles.container}>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>Tap</Text>
+            </View>
+
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>Tap</Text>
+            </View>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>me</Text>
+            </View>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>and</Text>
+            </View>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>scroll</Text>
+            </View>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>for</Text>
+            </View>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>more...</Text>
+            </View>
+            <View style={[styles.card,styles.cardElevated]}>
+                <Text>😊</Text>
+            </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 20,
+        marginTop:8,
+        marginHorizontal: 8,
+        fontWeight: 'bold'
+    },
+    container:{
+        padding:10,
+        
+    },
+    card:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        width:100,
+        height:100,
+        margin:8
+    },
+    cardElevated:{
+        backgroundColor:'lightblue',
+        elevation:9,
+        shadowOffset:{
+            width:1,
+            height:1
+        },
+        shadowColor:'red'
+    }
+})
